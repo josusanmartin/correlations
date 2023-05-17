69920a3e1c3d7b27b99f5db6aac89fd199ef4e97
Josu <josuanmartin@gmail.com>
2023-05-16 22:21:11 -0600

    Everything seems OK

--- main.py
+++ main.py
@@ -26,6 +26,14 @@ def cross_correlation(data, period):
 
 # Function to plot cross correlation
 def plot_correlation(data, assets, time_horizons):
+    time_horizons_names = {
+        30: '30 days',
+        60: '60 days',
+        90: '90 days',
+        180: '180 days',
+        360: '1 year',
+        1095: '3 years'
+    }
     for i in range(len(assets)-1):
         for j in range(i+1, len(assets)):
             fig = go.Figure()
@@ -34,12 +42,55 @@ def plot_correlation(data, assets, time_horizons):
                 corr = corr_data[assets[i]].unstack()[assets[j]].dropna()
                 fig.add_trace(go.Scatter(x=corr.index, y=corr,
                                          mode='lines',
-                                         name=f'{period} days'))
-            fig.update_layout(title=f'Cross Correlation of {assets[i]} vs {assets[j]}',
+                                         name=f'{time_horizons_names[period]}'))
+            fig.update_layout(title=f'Cross Correlation of {legend_names[assets[i]]} vs {legend_names[assets[j]]}',
                               xaxis_title='Date',
                               yaxis_title='Correlation')
             file_name = f"html/{assets[i].replace('^', '_').replace('=', '_')}_vs_{assets[j].replace('^', '_').replace('=', '_')}_correlation.html"
             plot(fig, filename = file_name, auto_open=False)
+
+# Function to plot cross correlation for all assets
+def all_plot_correlation(data, assets, time_horizons, legend_names):
+    # Mapping of time horizon in days to its equivalent in years
+    time_horizons_names = {
+        30: '30 days',
+        60: '60 days',
+        90: '90 days',
+        180: '180 days',
+        360: '1 year',
+        1095: '3 years'
+    }
+
+    # Only run for the first two assets
+    for asset in assets[:2]:
+        for period in time_horizons:
+            fig = go.Figure()
+            for other_asset in assets:
+                if asset != other_asset:
+                    corr_data = cross_correlation(data, period)
+                    corr = corr_data[asset].unstack()[other_asset].dropna()
+                    fig.add_trace(go.Scatter(x=corr.index, y=corr,
+                                             mode='lines',
+                                             name=legend_names[other_asset]))
+            print(time_horizons_names[period])
+            fig.update_layout(title=f'Cross Correlation of {legend_names[asset]} for {time_horizons_names[period]}',
+                              xaxis_title='Date',
+                              yaxis_title='Correlation')
+            file_name = f"html/{legend_names[asset]}_{period}_correlation.html"
+            plot(fig, filename = file_name, auto_open=False)
+
+
+# Define assets and display names
+assets = ['BTC-USD', 'ETH-USD', '^IXIC', '^GSPC', 'GC=F', 'DX-Y.NYB', 'EURUSD=X']
+legend_names = {
+    "BTC-USD": "BTCUSD",
+    "ETH-USD": "ETHUSD",
+    "^IXIC": "NASDAQ",
+    "^GSPC": "S&P",
+    "GC=F": "GOLD",
+    "DX-Y.NYB": "DXY",
+    "EURUSD=X": "EURUSD"
+}
 
 # Define assets and display names
 assets = ['BTC-USD', 'ETH-USD', '^IXIC', '^GSPC', 'GC=F', 'DX-Y.NYB', 'EURUSD=X']
@@ -51,6 +102,10 @@ with open('json/assets.json', 'w') as file:
     json.dump(assets, file)
 with open('json/display_names.json', 'w') as file:
     json.dump(display_names, file)
+
+# Save time horizons to JSON
+with open('json/time_horizons.json', 'w') as file:
+    json.dump(time_horizons, file)
 
 # Create a mapping from display names to ticker symbols
 name_to_symbol = {display: actual for display, actual in zip(display_names, assets)}
@@ -64,3 +119,4 @@ data = download_data(assets, start_date, end_date)
 
 # Calculate and plot cross correlation
 plot_correlation(data, assets, time_horizons)
+all_plot_correlation(data, assets, time_horizons, legend_names)
